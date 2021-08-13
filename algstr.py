6705ebc5d770178b5f6a0bf0a456a48a9751f958
Alex <roboshadow@mail.ru>
2021-08-13 22:06:05 +0300

    added deque, namedtuple

--- algstr.py
+++ algstr.py
@@ -433,20 +433,20 @@ matrix = [[random.randint(1, 10) for _ in range(size)] for _ in range(size)]
 #fib_list(500) - 999/1    0.000    0.000    0.000    0.000 algstr.py:411(_fib_list)
 
 # Фибоначчи через цикл
-def test_fib(func):
-    lst = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    for i, item in enumerate(lst):
-        assert item == func(i)
-        print(f'Test {i} Ok')
+# def test_fib(func):
+#     lst = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+#     for i, item in enumerate(lst):
+#         assert item == func(i)
+#         print(f'Test {i} Ok')
 
 
-def fib_loop(n):
-    if n < 2:
-        return n
-    first, second = 0, 1
-    for i in range(2, n + 1):
-        first, second = second, first + second
-    return second
+# def fib_loop(n):
+#     if n < 2:
+#         return n
+#     first, second = 0, 1
+#     for i in range(2, n + 1):
+#         first, second = second, first + second
+#     return second
 
 # print(test_fib(fib_loop))
 
@@ -459,11 +459,11 @@ def fib_loop(n):
 # cProfile.run('fib_loop(1000)')
 
 
-@functools.lru_cache()
-def fib(n):
-    if n < 2:
-        return 1
-    return fib(n-1) + fib(n-2)
+# @functools.lru_cache()
+# def fib(n):
+#     if n < 2:
+#         return 1
+#     return fib(n-1) + fib(n-2)
 
 # fib(10) - 1000 loops, best of 5: 101 nsec per loop
 # fib(100) - 1000 loops, best of 5: 91.6 nsec per loop
@@ -478,35 +478,226 @@ def fib(n):
 #fib(200) - 201/1    0.000    0.000    0.000    0.000 algstr.py:462(fib)
 
 # Модуль Collections
-from collections import Counter
+from collections import Counter, deque, defaultdict, OrderedDict, namedtuple
 
-a = Counter()
-b = Counter('abrakadabra')
-c = Counter({'red': 2, 'blue': 4})
-d = Counter(cat=4, dog=5)
-print(a, b, c, d, sep='\n')
-print(b['z'])
-b['z'] = 0
-print(b)
-print(list(b.elements()))
-print(b.most_common(2))
-q = Counter(a=4, b=6, c=-2, d=0)
-f= Counter(a=1, b=2, c=3, d=-2)
-q.subtract(f)
-print(q)
+# a = Counter()
+# b = Counter('abrakadabra')
+# c = Counter({'red': 2, 'blue': 4})
+# d = Counter(cat=4, dog=5)
+# print(a, b, c, d, sep='\n')
+# print(b['z'])
+# b['z'] = 0
+# print(b)
+# print(list(b.elements()))
+# print(b.most_common(2))
+# q = Counter(a=4, b=6, c=-2, d=0)
+# f= Counter(a=1, b=2, c=3, d=-2)
+# q.subtract(f)
+# print(q)
+# print('*' * 50)
+# print(set(q))
+# print(dict(q))
+# q.clear()
+# print(q)
+# print('*' * 50)
+# x = Counter(a=3, b=1)
+# y = Counter(a=1, b=2)
+# print(x + y)
+# print(x - y)
+# print(x & y)
+# print(x | y)
+# print('*' * 50)
+# z = Counter(a=2, b=-4)
+# print(+z)
+# print(-z)
+
+# Deque
+# a = deque()
+# b = deque('abcdef')
+# c = deque([1, 2, 3, 4, 5])
+# print(a, b, c, sep='\n')
+# b = deque('abcdef', maxlen=3)
+# c = deque([1, 2, 3, 4, 5], maxlen=4)
+# c.clear()
+# print(b, c, sep='\n')
+# Добавление в очередь
+# print('*' * 50)
+# d = deque([i for i in range(5)], maxlen=7)
+# d.append(5)
+# d.appendleft(6)
+# print(d)
+# d.extend([7, 8, 9])
+# print(d)
+# d.extendleft([10, 11, 12])
+# print(d)
+# Забрать элементы из очереди
+# print('*' * 50)
+# f = deque([i for i in range(5)], maxlen=7)
+# x = f.pop()
+# y = f.popleft()
+# print(f, x, y, sep='\n')
+# # Другие методы
+# print('*' * 50)
+# g = deque([i for i in range(5)], maxlen=7)
+# print(g.count(2))
+# print(g.index(3))
+# g.insert(2, 6)
+# print(g)
+# g.reverse()
+# print(g)
+# g.rotate(3)
+# print(g)
+# Пример использования
+# array = [random.randint(-100, 100) for _ in range(10)]
+# print(array)
+# deq = deque()
+# for item in array:
+#     if item > 0:
+#         deq.append(item)
+#     elif item < 0:
+#         deq.appendleft(item)
+# print(deq)
+
+# DefaultDict
+
+# a = defaultdict()
+# print(a)
+# s = 'ssdeegrhcxdceutigakqdkdgjxdce'
+# b = defaultdict(int)
+# for i in s:
+#     b[i] += 1
+# print(b)
+#
+# list_1 = [('cat', 1), ('dog', 5), ('cat', 2), ('mouse', 1), ('dog', 1)]
+# c = defaultdict(list)
+# for k, v in list_1:
+#     c[k].append(v)
+# print(c)
+#
+# list_2 = [('cat', 1), ('dog', 5), ('cat', 2), ('mouse', 1), ('dog', 1), ('dog', 5)]
+# d = defaultdict(set)
+# for k, v in list_1:
+#     d[k].add(v)
+# print(d)
+#
+# f = defaultdict(lambda: 'unknown')
+# f.update(rex='dog', thomas='cat')
+# print(f)
+# print(f['rex'])
+# print(f['jerry'])
+
+# OrderedDict
+# Сортируем по ключам
+# a = {'cat': 5, 'mouse': 4, 'dog': 2}
+# new_a = OrderedDict(sorted(a.items(), key=lambda x: x[0]))
+# print(new_a)
+# # Сортируем по значениям
+# b = {'cat': 5, 'mouse': 4, 'dog': 2}
+# new_b = OrderedDict(sorted(b.items(), key=lambda x: x[1]))
+# print(new_b)
+# print(new_a == new_b)
+#
+# new_b.move_to_end('mouse')
+# print(new_b)
+#
+# new_b.move_to_end('mouse', last=False)
+# print(new_b)
+
+# Удаляем последний элемент
+# new_b.popitem()
+# print(new_b)
+#
+# # Удаляем первый элемент
+# new_b.popitem(last=False)
+# print(new_b)
+#
+# new_b['cow'] = 1
+# print(new_b)
+# new_b['dog'] = 8
+# print(new_b)
+# print('*' * 50)
+#
+# new_c = OrderedDict(sorted(a.items(), key=lambda x: len(x[0])))
+# print(new_c)
+
+# for key in reversed(new_c):
+#     print(key, new_c[key])
+
+# Работа с файлом big_log.txt
+# N = 3000
+# with open('big_log.txt', 'r', encoding='utf-8') as f:
+#     log = deque(f, N)
+# print(log)
+#
+# data = OrderedDict()
+# spam = defaultdict(int)
+#
+# for item in log:
+#     ip = item[:-1]
+#     if not ip.startswith('192.168'):
+#         spam[ip] += 1
+#         data[ip] = 1
+# print(spam)
+# print(data)
+# data.update(spam)
+# print(data)
+# with open('data.txt', 'w', encoding='utf-8') as f:
+#     for key, value in data.items():
+#         f.write(f'{key} - {value}\n')
+
+# namedtuple
+
+hero_1 = ('Aaz', 'Izverg', 100, 0.0, 250)
+print(hero_1[4])
+
+
+class Person:
+
+    def __init__(self, name, race, health, mana, strength):
+        self.name = name
+        self.race = race
+        self.health = health
+        self.mana = mana
+        self.strength = strength
+
+
+hero_2 = Person('Aaz', 'Izverg', 100, 0.0, 250)
+print(hero_2.mana)
+
+New_Person = namedtuple('New_Person', 'name, race, health, mana, strength')
+hero_3 = New_Person('Aaz', 'Izverg', 100, 0.0, 250)
+print(hero_3)
+print(hero_3.race)
+
+prop = ['name', '3race', 'health', '_mana', 'strength']
+New_Person_1 = namedtuple('New_Person_1', prop, rename=True) # переименование недопустимых имен (цифры и т.п)
+hero_4 = New_Person_1('Aaz', 'Izverg', 100, 0.0, 250)
+print(hero_4)
+
 print('*' * 50)
-print(set(q))
-print(dict(q))
-q.clear()
-print(q)
+Point = namedtuple('Point', 'x, y, z')
+p1 = Point(2, z=3, y=4)
+print(p1)
+
+t = [5, 10, 15]
+p2 = Point._make(t)
+print(p2)
+d2 = p2._asdict()
+print(d2)
+
+p3 = p2._replace(x=6)
+print(p3)
+
+print(p3._fields)
+
 print('*' * 50)
-x = Counter(a=3, b=1)
-y = Counter(a=1, b=2)
-print(x + y)
-print(x - y)
-print(x & y)
-print(x | y)
-print('*' * 50)
-z = Counter(a=2, b=-4)
-print(+z)
-print(-z)
+
+New_Point = namedtuple('New_Point', 'x, y, z', defaults=[0, 0])
+p4 = New_Point(5)
+print(p4)
+
+print(p4._field_defaults)
+
+dct = {'x': 10, 'y': 20, 'z': 30}
+p5 = New_Point(**dct)
+print(p5)
